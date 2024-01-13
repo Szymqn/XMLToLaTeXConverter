c379c8dacb15f3ed43ea2743d595edc6179e25b6
szymqn <koodziejskisz@outlook.com>
2024-01-14 00:32:00 +0100

    Implemeted handling table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import numpy as np
 
 
 def xml_to_tex(xml_filename, tex_filename):
@@ -9,47 +10,35 @@ def xml_to_tex(xml_filename, tex_filename):
         tex_file.write('\\documentclass{article}\n')
         tex_file.write('\\begin{document}\n')
 
-        for plant in root.findall('.//PLANT'):
-            common = plant.find('COMMON').text
-            botanical = plant.find('BOTANICAL').text
-            zone = plant.find('ZONE').text
-            light = plant.find('LIGHT').text
-            price = plant.find('PRICE').text
-            availability = plant.find('AVAILABILITY').text
+        num_children = len(root)
+        total_grandchildren = sum(len(list(child)) for child in root)
+        each_grandchildren = int(total_grandchildren / num_children)
 
-            tex_file.write(f'\\section{{{common}}}\n')
-            tex_file.write(f'Botanical: {botanical}\n')
-            tex_file.write(f'Zone: {zone}\n')
-            tex_file.write(f'Light: {light}\n')
-            tex_file.write(f'Price: ${price}\n')
-            tex_file.write(f'Availability: {availability} units\n\n')
+        tex_file.write('\\begin{center}\n')
+        tex_file.write('\\begin{tabular}{|' + 'c|' * each_grandchildren + '}\n')
+        headers = []
+        for child in root:
+            for grandchild in child:
+                headers.append(grandchild.tag)
 
-        # for food in root.findall('.//FOOD'):
-        #     name = food.find('NAME').text
-        #     price = food.find('PRICE').text
-        #     description = food.find('DESCRIPTION').text
-        #     calories = food.find('CALORIES').text
-        #
-        #     tex_file.write(f'\\section{{{name}}}\n')
-        #     tex_file.write(f'Price: ${price}\n')
-        #     tex_file.write(f'Description: {description}\n')
-        #     tex_file.write(f'Calories: {calories} kcal\n\n')
+        headers = np.unique(headers)
 
-        # for book in root.findall('.//BOOK'):
-        #     author = book.find('AUTHOR').text
-        #     title = book.find('TITLE').text
-        #     genre = book.find('GENER').text
-        #     book_price = book.find('PRICE').text
-        #     date = book.find('DATE').text
-        #
-        #     tex_file.write(f'\\section{{{title}}}\n')
-        #     tex_file.write(f'Author: {author}\n')
-        #     tex_file.write(f'Genre: {genre}\n')
-        #     tex_file.write(f'Price: ${book_price}\n')
-        #     tex_file.write(f'Date: {date}\n\n')
+        tex_file.write('\hline\n')
+        for header in headers:
+            tex_file.write(f'{header} & ')
 
+        tex_file.write('\hline \hline\n')
+
+        for child in root:
+            for grandchild in child:
+                tex_file.write(f'{grandchild.text} & ')
+            tex_file.write('\\\\ \\hline\n')
+
+        tex_file.write('\\end{tabular}\n')
+        tex_file.write('\\end{center}\n')
         tex_file.write('\\end{document}\n')
-        print("Successful converted")
+
+    print("Successful conversion")
 
 
 if __name__ == "__main__":
