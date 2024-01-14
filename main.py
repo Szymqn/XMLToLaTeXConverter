d11062b86f75fcbe0a27a0655851f0952d0c4c13
szymqn <koodziejskisz@outlook.com>
2024-01-14 01:30:18 +0100

    Fixed table, added unordered and ordered lists

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-import numpy as np
 
 
 def xml_to_tex(xml_filename, tex_filename):
@@ -9,33 +8,70 @@ def xml_to_tex(xml_filename, tex_filename):
     with open(tex_filename, 'w') as tex_file:
         tex_file.write('\\documentclass{article}\n')
         tex_file.write('\\begin{document}\n')
+        tex_file.write('\\section{Table}\n')
 
-        num_children = len(root)
-        total_grandchildren = sum(len(list(child)) for child in root)
+        table_children = root[:3]
+        num_children = len(table_children)
+        total_grandchildren = sum(len(list(child)) for child in table_children)
         each_grandchildren = int(total_grandchildren / num_children)
 
         tex_file.write('\\begin{center}\n')
         tex_file.write('\\begin{tabular}{|' + 'c|' * each_grandchildren + '}\n')
+        tex_file.write('\hline\n')
+
         headers = []
-        for child in root:
+        for child in table_children:
             for grandchild in child:
                 headers.append(grandchild.tag)
 
-        headers = np.unique(headers)
+        headers = headers[:each_grandchildren]
 
-        tex_file.write('\hline\n')
-        for header in headers:
-            tex_file.write(f'{header} & ')
+        for i, header in enumerate(headers):
+            tex_file.write(header)
+            if i < len(headers) - 1:
+                tex_file.write(" & ")
+            else:
+                tex_file.write(" \\\\ \\hline \\hline\n")
 
-        tex_file.write('\hline \hline\n')
-
-        for child in root:
-            for grandchild in child:
-                tex_file.write(f'{grandchild.text} & ')
-            tex_file.write('\\\\ \\hline\n')
+        for child in table_children:
+            for i, grandchild in enumerate(child):
+                tex_file.write(f'{grandchild.text}')
+                if i < len(child) - 1:
+                    tex_file.write(' & ')
+                else:
+                    tex_file.write(' \\\\ \\hline\n')
 
         tex_file.write('\\end{tabular}\n')
         tex_file.write('\\end{center}\n')
+
+        tex_file.write('\\section{Unordered list}\n')
+
+        tex_file.write('\\begin{itemize}\n')
+        list_children = root[3:6]
+
+        for child in list_children:
+            temp = []
+            for i, grandchild in enumerate(child):
+                temp.append(grandchild.text)
+            my_string = ', '.join(map(str, temp))
+            tex_file.write(f'\\item {my_string}\n')
+
+        tex_file.write('\\end{itemize}\n')
+
+        tex_file.write('\\section{Ordered list}\n')
+
+        tex_file.write('\\begin{enumerate}\n')
+        list_children = root[6:9]
+
+        for child in list_children:
+            temp = []
+            for i, grandchild in enumerate(child):
+                temp.append(grandchild.text)
+            my_string = ', '.join(map(str, temp))
+            tex_file.write(f'\\item {my_string}\n')
+
+        tex_file.write('\\end{enumerate}\n')
+
         tex_file.write('\\end{document}\n')
 
     print("Successful conversion")
